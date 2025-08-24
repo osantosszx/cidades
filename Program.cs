5edc7aa7b55aed9e16bdf7f5b35d311547a546b7
Guilherme santos <guiss250507@gmail.com>
2025-08-24 17:12:05 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Bertioga");
